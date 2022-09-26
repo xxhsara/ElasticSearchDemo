1ad7862d3365c542fea74d6cef101490c2f1a805
xxhsara <2103309470@qq.com>
2022-09-26 22:48:21 +0800

    Elastic CodeInit

---
+++ ElasticSearchDemo/ElasticSearchDemo/ElasticSearchUtilty.cs
@@ -0,0 +1,108 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticSearchDemo
+{
+    public class ElasticSearchUtilty
+    {
+        //在kibana中查询 GET /order/_doc/_search
+        public static void InsertObject(ElasticClient client)
+        {
+            //var newInfo = new
+            //{
+            //    Id="001",
+            //    Name="张三"
+            //};
+
+            //IndexResponse response = client.IndexDocument(newInfo);
+            //if(response.IsValid)
+            //{
+            //    Console.WriteLine("对象添加成功");
+            //}
+
+
+            //批量新增
+            var infoList = new List<object>
+            {
+                new
+                {
+                     Id="002",
+                     Name="李四"
+                },
+                 new
+                {
+                     Id="003",
+                     Name="王五"
+                },
+                  new
+                {
+                     Id="004",
+                     Name="刘二"
+                },
+                   new
+                {
+                     Id="005",
+                     Name="赵四"
+                },
+                   new
+                {
+                     Id="006",
+                     Name="李丽"
+                },
+            };
+
+            BulkResponse response1 = client.IndexMany(infoList);
+
+            if (response1.IsValid)
+            {
+                Console.WriteLine("批量新增成功");
+            }
+        }
+
+
+        public static void Search(ElasticClient client)
+        {
+            var searchAllResponse = client.Search<Order>();
+            IReadOnlyCollection<Order> allorderList = searchAllResponse.Documents;
+            foreach (var item in allorderList)
+            {
+                Console.WriteLine($"id:{item.Id},name:{item.Name}");
+            }
+
+            Console.WriteLine("限定条件查询");
+
+            var searchResponse = client.Search<Order>(s=>s.From(0).Size(3)
+            .Query(q=>q.Match(m=>m.Field(f=>f.Name.Contains('李')))));
+            IReadOnlyCollection<Order> orderList = searchResponse.Documents;
+            foreach (var item in orderList)
+            {
+                Console.WriteLine($"id:{item.Id},name:{item.Name}");
+            }
+        }
+
+        public static void Update(ElasticClient client)
+        {
+
+        }
+
+        public static void Delete(ElasticClient client)
+        {
+            client.Delete(new DocumentPath<Order>("002"), s =>
+            {
+                DeleteRequest request = new DeleteRequest("order", "002");
+                return request;
+            });
+            Console.WriteLine("删除成功");
+            var searchAllResponse = client.Search<Order>();
+            IReadOnlyCollection<Order> allorderList = searchAllResponse.Documents;
+            foreach (var item in allorderList)
+            {
+                Console.WriteLine($"id:{item.Id},name:{item.Name}");
+            }
+        }
+    }
+}
---
+++ ElasticSearchDemo/ElasticSearchDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ElasticSearchDemo;
+using Nest;
+
+Console.WriteLine("Hello, World!");
+
+ConnectionSettings setting = new ConnectionSettings(new Uri("http://192.168.10.100:9200/")).DefaultIndex("order");//在ES中索引相当于数据库
+
+ElasticClient client = new ElasticClient(setting);
+
+ElasticSearchUtilty.InsertObject(client);
+
+ElasticSearchUtilty.Search(client);
+
+ElasticSearchUtilty.Delete(client);
+
+Console.ReadLine();
+
+
